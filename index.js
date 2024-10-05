cc75b6a2d9674a41c517e98df8dcb1c90aa2d7b9
kokolopo <kokolopo321@gmail.com>
2024-10-05 19:53:21 +0700

    logs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import router from "./src/routes/index.js";
+import morgan from "morgan";
+import moment from "moment";
 
 const port = 4321;
 const app = express();
@@ -9,6 +11,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+
+// morgan logs
+// Buat token untuk waktu aksi
+morgan.token("action-time", function (req, res) {
+  return moment().format("YYYY-MM-DD HH:mm:ss");
+});
+
+// Buat token untuk IP pengakses
+morgan.token("remote-addr", function (req, res) {
+  return req.ip;
+});
+
+// Buat token untuk body request
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+// Use morgan middleware with custom format that includes the body
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :body :action-time :remote-addr"
+  )
+);
 
 app.use(router);
 
